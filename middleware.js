681a02964df53db4fb46dbcb5c4c5ecfeb590bb0
DESKTOP-JRPF1OL\gooka_9jgac6y <nutmakchine.n@gmail.com>
2024-10-25 00:01:48 +0700

    group  page in (page)

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { withAuth } from "next-auth/middleware";
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  //   return NextResponse.redirect(new URL('/home', request.url))
+    // return NextResponse.redirect('/')
  
 }
 // See "Matching Paths" below to learn more
--- src/app/layouts/MainLayout.jsx
+++ src/app/layouts/MainLayout.jsx
@@ -9,16 +9,6 @@ import SideNavigation from './SideNavigation';
 
 import ProtectedPage from '../components/protectedPage';
 
-const geistSans = localFont({
-    src: "../fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "../fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
 
 export default function RootLayout({ children }) {
 
